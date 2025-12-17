7aa9a600b407fb12c8cd7d2af01588b4b547e446
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-17 15:16:28 +0530

    backendexp

---
+++ webexep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERNAME=$(userid -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+USERID() {
+    if [ $USERNAME -ne 0 ]; then
+       echo -e "you need root access to  $Y continue"
+       exit 1
+    fi
+}
+
+USERID
