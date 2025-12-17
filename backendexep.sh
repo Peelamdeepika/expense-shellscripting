1accc717412d93f01056f64fdf888f4881a44be0
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-17 15:08:17 +0530

    backendexp

--- backendexep.sh
+++ backendexep.sh
@@ -8,7 +8,7 @@ Y="\e[33m"
 
 USERID() {
     if [ $USERNAME -ne 0 ]; then
-      echo "you need root access to $Y continue"
+      echo -e "you need root access to $Y continue"
       exit 1
     fi
 }
