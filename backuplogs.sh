02aaa4aa16fa806d1a8569dc5fb70ffb990f593b
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-19 15:34:19 +0530

    backendexp

--- backuplogs.sh
+++ backuplogs.sh
@@ -1,50 +1,28 @@
 #!/bin/bash
 
 SOURCE_DIR=$1
-DESTI_DIR=$2
-DAYS=${3:-0}
-
+DEST_DIR=$2
+DAYS=${3:-14}
 
 
 if [ $# -lt 2 ]
-  then
-  echo "source or destination dircetory is missing"
+  then 
+  echo " you should give <SOURCE_DIR> <DEST_DIR> <DAYS>"
   exit 1
-fi 
-
- if [ ! -d "$SOURCE_DIR" ] 
-    then
-     echo " not able to find $SOURCE_DIR"
-     exit 1
 fi
 
-if [ ! -d "$DESTI_DIR" ]
-  then 
-     echo " not able to find $DESTI_DIR"
-     exit 1
+if [ ! -d $SOURCE_DIR ]
+   then 
+   echo "source dir is not available"
+   exit 1
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mmin +$DAYS)
-
-if [ -n "$FILES" ]
-  then 
-  echo " we are able to find files $FILES"
-  ZIP_FILE="/home/ec2-user/arch_log/app_logs.zip"
-  echo "$FILES" | zip -@ "$ZIP_FILE"
-if [ -f $ZIP_FILE ]
- then
-   echo " successfully created the $ZIP_FILE file"
-  while read -r filepath
-  do 
-      echo "Deleting the $filepath"
-      rm -rf "$filepath"
-      echo "Deleted the $filepath"
-  done <<< $FILES
-
- else
- echo "failed to create zipfile"
- exit 1
+if [ ! -d $DEST_DIR ]
+   then 
+   echo "destination dir is not available"
+   exit 1
 fi
-else
-echo "we are not able to find files"
-fi
+
+
+
+
