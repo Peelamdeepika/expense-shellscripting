15890705117171937f1857e7b00425d5efcbcd75
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-19 13:08:58 +0530

    backendexp

--- backuplogs.sh
+++ backuplogs.sh
@@ -7,16 +7,19 @@ DAYS={3:-14}
 
 
 if [ $# -lt 2 ]
+  then
   echo "source or destination dircetory is missing"
   eit 1
 fi 
 
  if [ ! -d "$SOURCE_DIR" ] 
+    then
      echo " not able to find $SOURCE_DIR"
      exit 1
 fi
 
-if [ ! -d "$DESTI_DIR" ] 
+if [ ! -d "$DESTI_DIR" ]
+  then 
      echo " not able to find $DESTI_DIR"
      exit 1
 fi
